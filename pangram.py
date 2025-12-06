ead3bc53000569c61bd5224aa573b72db70bae94
jai <jaisankarpalla36@gmail.com>
2025-12-06 16:42:06 +0530

    pangram 520A solution

---
+++ pangram.py
@@ -0,0 +1,29 @@
+# my solution 
+import string
+lis = list(string.ascii_lowercase)
+# print(lis)
+
+def check(word):
+    for i in lis:
+        if i not in word:
+            return False
+        else:
+            continue
+    return True
+        
+n = int(input())
+word = input().lower()
+
+if check(word):
+    print("YES")
+else:
+    print("NO")
+
+# another solution - common sense
+# n = int(input())
+# word = input().lower()
+
+# if len(set(word)) == 26:
+#     print("YES")
+# else:
+#     print("NO")

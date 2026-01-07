951167cb14f7a8d43ba1937243ed0b12f2fe2bc3
jai <jaisankarpalla36@gmail.com>
2026-01-07 19:19:27 +0530

    solution for 705A - Hulk

--- hulk.py
+++ hulk.py
@@ -1,22 +1,12 @@
 n = int(input())
 
 lis = []
-if n == 1:
-    print("I hate it")
-# elif n == 2:
-#     print("I love it")
-else:
-    for i in range(1, n + 1):
-        if i == n:
-            if i % 2 == 0: 
-                lis.append("I love it")
-                break
-            else:
-                lis.append("I hate it")
-                break
-        
+
+
+for i in range(1, n + 1):
         if i % 2 == 0: 
             lis.append("I love")
         else:
             lis.append("I hate")
-    print(" that ".join(lis))
+
+print(" that ".join(lis) + " it")
